850f61687d6d8c55b0efffaaf219470e21f7e287
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-08-19 13:30:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+// const log = console.log;
+var customerName = 'bob'
+// let bestCustomer;
+const leastFavoriteCustomer = 'Queen';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+// log(upperCaseCustomerName());
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+// log(setBestCustomer());
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return ;
+}
+// log(overwriteBestCustomer());
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = '';
+    return leastFavoriteCustomer;
+}
+changeLeastFavoriteCustomer();
